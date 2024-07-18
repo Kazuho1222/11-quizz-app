407619ef5da76f075582ee2e29fb295a238d6c61
Kazuho1222 <destiny5914@gmail.com>
2024-07-19 08:12:32 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FaRegHand } from 'react-icons/fa6'
+import './App.css'
+
+function App() {
+
+  return (
+    <button className='bg-red-600 p-4 text-white rounded-full'>
+      <FaRegHand />
+    </button>
+
+  )
+}
+
+export default App
